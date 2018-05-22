c6490fb1923f943faf0ba53e4072fe05e24d85c3
“Nursultan <nmakhamatov@tsconsulting.ru>
2018-05-22 18:22:00 +0300

    Изменил метод пересчёта зп в случае добавления группы сотрудников(раньше работала неправильно) Изменил способ группировки сотрудников

--- src/main/java/ru/tsconsulting/Internship/PrintToFile.java
+++ src/main/java/ru/tsconsulting/Internship/PrintToFile.java
@@ -27,23 +27,31 @@ public class PrintToFile {
                     BigDecimal averSalary = map.get(departmentName).countAverSalary();
                     for (Person[] persons : combinations.get(departmentName)) {
                         BigDecimal sum = BigDecimal.ZERO;
-                        StringBuilder employees = new StringBuilder();
+
                         for (Person per : persons) {
                             sum = sum.add(per.getSalary());
-                            employees = employees.append(per.getName());
-                            employees = employees.append(",");
+//                            employees = employees.append(per.getName());
+//                            employees = employees.append(",");
                         }
-                        employees.deleteCharAt(employees.length()-1);
+//                        employees.deleteCharAt(employees.length()-1);
                         sum = sum.divide(new BigDecimal(persons.length), 2);
+                        StringBuilder employees = new StringBuilder();
                         for (Map.Entry<String, Department> deps : map.entrySet()) {
                             BigDecimal averSalary2 = deps.getValue().countAverSalary();
                             if ((sum.compareTo(averSalary) < 0) && (sum.compareTo(averSalary2) > 0)) {
+                                for (Person per : persons) {
+//                                    sum = sum.add(per.getSalary());
+                                    employees = employees.append(per.getName());
+                                    employees = employees.append(",");
+                                }
+                                employees.deleteCharAt(employees.length()-1);
                                 pw.println("Сотрудники: " + employees + " могут перейти из отдела " +
                                         departmentName + " со средней зп " + averSalary + " в отдел "
                                         + deps.getKey() + " со средней зп (старая:" + averSalary2 +
-                                        " => новая:" + addAndRecountSalary(sum,averSalary2,
-                                        deps.getValue().getListOfPersons().size())   + ")");
+                                        " => новая:" + addAndRecountSalary(deps.getValue().getListOfPersons(),persons) + ")");
+                                employees = employees.delete(0,employees.length());
                             }
+
                         }
                     }
                     combinations.remove(departmentName);
@@ -55,9 +63,16 @@ public class PrintToFile {
         }
         System.out.println("Результаты сохранены в:" + folder + "Out.txt");
     }
-    private BigDecimal addAndRecountSalary(BigDecimal addingSalary,BigDecimal prevAverSalary,int amount) {
-        BigDecimal result = addingSalary.add(prevAverSalary.negate());
-        result = result.divide(new BigDecimal(amount+1),2,BigDecimal.ROUND_HALF_UP);
+    // Метод пересчёта средней зп отдела в случае добавления группы сотрудников
+    private BigDecimal addAndRecountSalary(List<Person> list, Person[] persons) {
+        BigDecimal result = BigDecimal.ZERO;
+        for (Person p:list) {
+            result = result.add(p.getSalary());
+        }
+        for (Person p2:persons) {
+            result = result.add(p2.getSalary());
+        }
+        result = result.divide(new BigDecimal(list.size() + persons.length),2,BigDecimal.ROUND_HALF_UP);
         return result;
     }
 }
